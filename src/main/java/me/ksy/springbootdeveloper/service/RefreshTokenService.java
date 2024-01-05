79a5b9c14fa6caf711cdf37ebb64283882bce452
Kseoyeon <kseoyeoni98@gmail.com>
2024-01-05 16:53:25 +0900

    project init

---
+++ src/main/java/me/ksy/springbootdeveloper/service/RefreshTokenService.java
@@ -0,0 +1,17 @@
+package me.ksy.springbootdeveloper.service;
+
+import lombok.RequiredArgsConstructor;
+import me.ksy.springbootdeveloper.domain.RefreshToken;
+import me.ksy.springbootdeveloper.repository.RefreshTokenRepository;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class RefreshTokenService {
+    private final RefreshTokenRepository refreshTokenRepository;
+
+    public RefreshToken findByRefreshToken(String refreshToken) {
+        return refreshTokenRepository.findByRefreshToken(refreshToken)
+                .orElseThrow(() -> new IllegalArgumentException("Unexpected token"));
+    }
+}
